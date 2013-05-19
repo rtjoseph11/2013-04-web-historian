1e9755644325a6fa56f5175d55388050295af5ea
Hack Reactor Students <github@hackreactor.com>
2013-05-19 12:26:37 -0700

    now using generic http request rather than http-get library

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,22 +1,50 @@
 var fs = require('fs');
-var getter = require('http-get');
+var http = require('http');
+var url = require('url');
 exports.readUrls = function(filePath, cb){
   fs.readFile(filePath, 'utf8', function(err,data){
     if (err){
       console.log('there was an error!', err);
     } else {
      var urlArray = data.split('\n');
+     urlArray.splice(urlArray.length - 1, 1);
      urlArray.forEach(cb);
     }
   });
 };
 
 exports.downloadUrl = function(url){
-    getter.get(url, __dirname + '/../../data/sites/' + url, function(err, result){
-      if (err){
-        console.error(err);
-      } else {
-        console.log('File downloaded at: ' + result.file);
-      }
+    var options = {
+      hostname: url,
+      port: 80,
+      path: '/',
+      method: 'GET'
+    };
+    var req = http.request(options, function(res){
+      var body = "";
+      console.log(url,' STATUS: ' + res.statusCode);
+      res.setEncoding('utf8');
+      res.on('data', function(data){
+        body += data;
+      });
+      res.on('end', function(){
+        console.log(res.url);
+        fs.writeFile(__dirname + '/../../data/sites/' + url, body, function(err){
+          if (err){
+            console.error(err);
+          } else {
+            console.log('File downloaded at: ' + __dirname + '/../../data/sites/' + url);
+          }
+        });
+      });
     });
+    req.on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
+    req.end();
 };
+
+
+
+
+
